6659f7824ff17451ed9f8b049c6d4a9fe2434fb8
Arijus <aryjeleng@gmail.com>
2022-11-03 22:50:29 +0000

    Removed unnecessary styles

--- screens/QuizCreator.js
+++ screens/QuizCreator.js
@@ -2,11 +2,10 @@ import {
     SafeAreaView,
     View,
     Text,
-    Image,
     StyleSheet,
     Button,
   } from "react-native";
-  import { COLOURS, assets } from "../constants";
+  import { COLOURS } from "../constants";
   
   // Component imports
 import SimpleHeader from "../components/SimpleHeader";
@@ -66,25 +65,6 @@ import SimpleHeader from "../components/SimpleHeader";
       alignItems: "center",
     },
   
-    newQuizButton: {
-      width: 67,
-      height: 67,
-      backgroundColor: COLOURS.white,
-      alignItems: "center",
-      justifyContent: "center",
-      borderRadius: 100,
-      position: "absolute",
-      top: 130,
-      left: 30,
-    },
-  
-    newQuizButtonIcon: {
-      width: 60,
-      height: 60,
-      bottom: -3,
-      right: -1,
-    },
-  
     sectionContent: {
       top: 234,
       width: "100%",
@@ -95,72 +75,6 @@ import SimpleHeader from "../components/SimpleHeader";
       fontFamily: "HammersmithOne",
       fontSize: 20,
       textAlign: "center",
-    },
-  
-    quizList: {
-      marginTop: 28,
-    },
-  
-    quizListItem: {
-      height: 102,
-      marginBottom: 24,
-      borderRadius: 20,
-      padding: 12,
-    },
-  
-    quizTitle: {
-      fontFamily: "HammersmithOne",
-      fontSize: 20,
-      marginBottom: 18,
-    },
-  
-    cardBottomContainer: {
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "space-between",
-    },
-  
-    lastAccessedText: {
-      color: COLOURS.secondaryText,
-      fontFamily: "HammersmithOne",
-      fontSize: 14,
-      width: 160,
-    },
-  
-    deleteButton: {
-      borderRadius: 100,
-      backgroundColor: COLOURS.trashRed,
-      alignItems: "center",
-      justifyContent: "center",
-      padding: 8,
-    },
-  
-    trashIcon: {
-      width: 20,
-      height: 20,
-    },
-  
-    shadow: {
-      shadowColor: COLOURS.black,
-      shadowOffset: {
-        width: 0,
-        height: 10,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.5,
-      elevation: 5,
-    },
-  
-    shadowDark: {
-      shadowColor: COLOURS.black,
-      shadowOffset: {
-        width: 0,
-        height: 7,
-      },
-      shadowOpacity: 0.41,
-      shadowRadius: 9.11,
-  
-      elevation: 14,
     },
   });
   
