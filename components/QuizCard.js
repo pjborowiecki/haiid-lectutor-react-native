c1eda8dcc3654c022e4a86f55f89abc6ef2bf435
P J Borowiecki <piotr.borowiecki@yahoo.com>
2022-11-07 14:09:34 +0100

    Style QuestionLoadingScreen, QuestionReview screen, and Flashcard component

--- components/QuizCard.js
+++ components/QuizCard.js
@@ -5,108 +5,103 @@ import { COLOURS, assets } from "../constants";
 export default function QuizCard({ navigation, quiz, onDelete }) {
   return (
     <TouchableOpacity
-        style={{
-            ...styles.quizListItem,
-            ...styles.shadow,
-            backgroundColor: quiz.colour,
-        }}
-        onPress={() => navigation.navigate(
-            "QuizPlay", 
-            {id: quiz.id}
-        )}
+      style={{
+        ...styles.quizListItem,
+        ...styles.shadow,
+        backgroundColor: quiz.colour,
+      }}
+      onPress={() => navigation.navigate("QuizPlay", { id: quiz.id })}
     >
-        {/* Quiz Title */}
-        <View>
-            <Text style={styles.quizTitle}>{quiz.name}</Text>
-        </View>
+      {/* Quiz Title */}
+      <View>
+        <Text style={styles.quizTitle}>{quiz.name}</Text>
+      </View>
 
-        {/* Card Bottom Container */}
-        <View style={styles.cardBottomContainer}>
+      {/* Card Bottom Container */}
+      <View style={styles.cardBottomContainer}>
         {/* Last Accessed */}
         <Text style={styles.lastAccessedText}>
-            Last accessed on{" "}
-            <Text style={{ color: COLOURS.black }}>
-            {quiz.lastAccessed}
-            </Text>
+          Last accessed on{" "}
+          <Text style={{ color: COLOURS.black }}>{quiz.lastAccessed}</Text>
         </Text>
 
         {/* Delete Button */}
-        <TouchableOpacity 
+        <TouchableOpacity
           style={styles.deleteButton}
           onPress={() => onDelete(quiz)}
         >
-            <Image
-                source={assets.trashIcon}
-                resizeMode="contain"
-                style={styles.trashIcon}
-            />
+          <Image
+            source={assets.trashIcon}
+            resizeMode="contain"
+            style={styles.trashIcon}
+          />
         </TouchableOpacity>
-        </View>
+      </View>
     </TouchableOpacity>
   );
 }
 
 // Styles
 const styles = StyleSheet.create({
-    quizListItem: {
-        height: 102,
-        marginBottom: 24,
-        borderRadius: 20,
-        padding: 12,
-      },
-    
-      quizTitle: {
-        fontFamily: "HammersmithOne",
-        fontSize: 20,
-        marginBottom: 18,
-      },
-    
-      cardBottomContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-      },
-    
-      lastAccessedText: {
-        color: COLOURS.secondaryText,
-        fontFamily: "HammersmithOne",
-        fontSize: 14,
-        width: 160,
-      },
-    
-      deleteButton: {
-        borderRadius: 100,
-        backgroundColor: COLOURS.trashRed,
-        alignItems: "center",
-        justifyContent: "center",
-        padding: 8,
-      },
-    
-      trashIcon: {
-        width: 20,
-        height: 20,
-      },
-    
-      shadow: {
-        shadowColor: COLOURS.black,
-        shadowOffset: {
-          width: 0,
-          height: 10,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.5,
-        elevation: 5,
-      },
-    
-      shadowDark: {
-        shadowColor: COLOURS.black,
-        shadowOffset: {
-          width: 0,
-          height: 7,
-        },
-        shadowOpacity: 0.41,
-        shadowRadius: 9.11,
-    
-        elevation: 14,
-      },
+  quizListItem: {
+    height: 102,
+    marginBottom: 24,
+    borderRadius: 20,
+    padding: 12,
+  },
+
+  quizTitle: {
+    fontFamily: "HammersmithOne",
+    fontSize: 20,
+    marginBottom: 18,
+  },
+
+  cardBottomContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+
+  lastAccessedText: {
+    color: COLOURS.secondaryText,
+    fontFamily: "HammersmithOne",
+    fontSize: 14,
+    width: 160,
+  },
+
+  deleteButton: {
+    borderRadius: 100,
+    backgroundColor: COLOURS.trashRed,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+  },
+
+  trashIcon: {
+    width: 20,
+    height: 20,
+  },
+
+  shadow: {
+    shadowColor: COLOURS.black,
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5,
+  },
+
+  shadowDark: {
+    shadowColor: COLOURS.black,
+    shadowOffset: {
+      width: 0,
+      height: 7,
+    },
+    shadowOpacity: 0.41,
+    shadowRadius: 9.11,
+
+    elevation: 14,
+  },
 });
