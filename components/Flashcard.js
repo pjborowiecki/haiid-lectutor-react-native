b1e0ac877a6cbfde83af2c6db30242f3bfa55e3a
P J Borowiecki <piotr.borowiecki@yahoo.com>
2022-11-07 14:09:34 +0100

    Style QuestionLoadingScreen, QuestionReview screen, and Flashcard component

--- components/Flashcard.js
+++ components/Flashcard.js
@@ -1,114 +1,177 @@
-import {
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import { COLOURS } from "../constants";
+import { Text, Image, StyleSheet, TouchableOpacity, View } from "react-native";
+import { COLOURS, assets } from "../constants";
 
 // Component imports
 import { TextInput } from "react-native-gesture-handler";
 import { useState } from "react";
 
 // Flashcard component
-export default function Flashcard({ 
-  flashcard, 
-  size, 
+export default function Flashcard({
+  flashcard,
+  size,
   index,
-  playingQuiz=false, 
-  quizId=null,
-  onNext=() => {}, 
-  onPrev=() => {},
-  onReroll=() => {}, 
-  onDelete=() => {},
+  playingQuiz = false,
+  quizId = null,
+  onNext = () => {},
+  onPrev = () => {},
+  onReroll = () => {},
+  onDelete = () => {},
 }) {
-
   const [revealAnswer, setRevealAnswer] = useState(false);
   const [revealedAnswerOnce, setRevealedAnswerOnce] = useState(false);
   const [answer, setAnswer] = useState("");
 
   const flipCard = () => {
-      if (playingQuiz) {
-          setRevealAnswer(!revealAnswer);
-      }
-  }
+    if (playingQuiz) {
+      setRevealAnswer(!revealAnswer);
+    }
+  };
 
   const nextCard = () => {
     setRevealedAnswerOnce(revealAnswer || revealedAnswerOnce ? true : false);
     onNext(revealAnswer || revealedAnswerOnce ? true : false);
     setRevealAnswer(false);
-  }
+  };
 
   const prevCard = () => {
     setRevealedAnswerOnce(revealAnswer || revealedAnswerOnce ? true : false);
     onPrev(revealAnswer || revealedAnswerOnce ? true : false);
     setRevealAnswer(false);
-  }
+  };
 
   return (
-    <View style={styles.homeScreenWrapper}>
-
-        {/* Flashcard question */}
-        { playingQuiz && !revealAnswer && <TouchableOpacity onPress={flipCard}>
+    <View style={styles.flashCardWrapper}>
+      {/* Flashcard question */}
+      {playingQuiz && !revealAnswer && (
+        <TouchableOpacity onPress={flipCard}>
           <Text>{flashcard.question}</Text>
-        </TouchableOpacity>}
-        
-        {/* Flashcard answer */}
-        { playingQuiz && revealAnswer && <TouchableOpacity onPress={flipCard}>
+        </TouchableOpacity>
+      )}
+
+      {/* Flashcard answer */}
+      {playingQuiz && revealAnswer && (
+        <TouchableOpacity onPress={flipCard}>
           <Text>{flashcard.answer}</Text>
-        </TouchableOpacity>}
+        </TouchableOpacity>
+      )}
 
-        { !playingQuiz && <View>
-          <Text>{flashcard.question}</Text>
-        </View>}
-        
-        {/* Input for typing in answer */}
-        { playingQuiz && <TextInput
+      {/* Input for typing in answer */}
+      {playingQuiz && (
+        <TextInput
           value={answer}
-          onChange={value => setAnswer(value)}
+          onChange={(value) => setAnswer(value)}
           placeholder="Enter your answer (optional):"
-        />}
+        />
+      )}
 
-        {/* Question counter */}
-        { playingQuiz && <Text>{index}/{size}</Text>}
+      {/* Question counter */}
+      {playingQuiz && (
+        <Text>
+          {index}/{size}
+        </Text>
+      )}
 
-        {/* Next/Previous buttons */}
-        { playingQuiz && <View>
-          {index > 1 && <TouchableOpacity 
-              onPress={() => {setAnswer(""); prevCard()}}
-          >
-            <Text>Prev</Text>
-          </TouchableOpacity>}
-          {index < size && <TouchableOpacity 
-              onPress={() => {setAnswer(""); nextCard()}}
-          >
-            <Text>Next</Text>
-          </TouchableOpacity>}
-        </View>}
-        
-        {/* Reroll and delete buttons */}
-        { !playingQuiz && <View>
+      {/* Next/Previous buttons */}
+      {playingQuiz && (
+        <View>
+          {index > 1 && (
             <TouchableOpacity
-              onPress={() => onReroll(flashcard.id, quizId)}>
-              <Text>Reroll</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => onDelete(flashcard.id, quizId)}
+              onPress={() => {
+                setAnswer("");
+                prevCard();
+              }}
             >
-              <Text>Delete</Text>
+              <Text>Prev</Text>
             </TouchableOpacity>
-        </View>}
+          )}
+          {index < size && (
+            <TouchableOpacity
+              onPress={() => {
+                setAnswer("");
+                nextCard();
+              }}
+            >
+              <Text>Next</Text>
+            </TouchableOpacity>
+          )}
+        </View>
+      )}
+
+      {/* REWRITE FLASHCARd SCREEN */}
+      {!playingQuiz && (
+        <View style={styles.rewriteCardWrapper}>
+          <View style={styles.rewriteCardTop}>
+            <TouchableOpacity onPress={() => onReroll(flashcard.id, quizId)}>
+              <Image
+                source={assets.newQuestionArrowsIcon}
+                style={styles.cardIcon}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.rewriteCardMiddle}>
+            <Text style={styles.cardText}>{flashcard.question}</Text>
+          </View>
+          <View style={styles.rewriteCardBottom}>
+            <TouchableOpacity onPress={() => onDelete(flashcard.id, quizId)}>
+              <Image source={assets.trashIcon} style={styles.cardIcon} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
     </View>
   );
 }
-  
-  // Styles
-  const styles = StyleSheet.create({
-    homeScreenWrapper: {
-      backgroundColor: COLOURS.white,
-      height: "50%",
-      display: "flex",
-      alignItems: "center",
-    },
-  });
-  
+
+// Styles
+const styles = StyleSheet.create({
+  flashCardWrapper: {
+    backgroundColor: COLOURS.rewriteFlashcardBg,
+    height: 168,
+    width: 300,
+    borderColor: COLOURS.black,
+    borderWidth: 1,
+    borderRadius: 10,
+    // padding: 16,
+
+    marginTop: 24,
+    display: "flex",
+    alignItems: "center",
+  },
+
+  rewriteCardWrapper: {
+    display: "flex",
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    width: "100%",
+    height: "100%",
+    padding: 12,
+  },
+
+  rewriteCardTop: {
+    display: "flex",
+    alignItems: "flex-end",
+  },
+
+  rewriteCardMiddle: {
+    display: "flex",
+    alignContent: "center",
+    alignItems: "center",
+  },
+
+  rewriteCardBottom: {
+    display: "flex",
+    alignItems: "flex-end",
+  },
+
+  cardIcon: {
+    width: 24,
+    height: 24,
+  },
+
+  cardText: {
+    fontFamily: "HammersmithOne",
+    fontSize: 24,
+    textAlign: "center",
+  },
+});
--- screens/quiz/QuizLoadingScreen.js
+++ screens/quiz/QuizLoadingScreen.js
@@ -1,67 +1,95 @@
-  import {
-      SafeAreaView,
-      View,
-      Text,
-      StyleSheet,
-    } from "react-native";
-  import { COLOURS } from "../../constants";
-  import { useEffect, useState } from "react";
+import { SafeAreaView, View, Text, StyleSheet } from "react-native";
+import { COLOURS } from "../../constants";
+import { useEffect, useState } from "react";
 
-  // Import components
-  import { Bar } from "react-native-progress";
+// Import components
+import { Bar } from "react-native-progress";
 
-  // Loading Screen screen
-  export default function QuizLoadingScreen({ navigation, route }) {
+// Loading Screen screen
+export default function QuizLoadingScreen({ navigation, route }) {
+  const [progress, setProgress] = useState(0);
 
-    const [progress, setProgress] = useState(0);
+  useEffect(() => {
+    if (progress === 1) {
+      setTimeout(() => {
+        navigation.navigate("QuestionReview", { id: route.params.id });
+      }, 250);
+    }
+    setTimeout(() => {
+      const newProgress = progress + Math.random() / 1.5;
+      setProgress(newProgress > 1 ? 1 : newProgress);
+    }, 500);
+  }, [progress]);
 
-    useEffect(() => {
-        if (progress === 1) {
-            setTimeout(() => {
-                navigation.navigate(
-                  "QuestionReview", 
-                  {id: route.params.id
-                });
-            }, 250)
-        }
-        setTimeout(() => {
-            const newProgress = progress + Math.random()/1.5;
-            setProgress(newProgress > 1 ? 1 : newProgress);
-        }, 500)
-    }, [progress])
+  return (
+    <SafeAreaView style={styles.homeScreenWrapper}>
+      {/* Section Content */}
+      <View style={styles.sectionContent}>
+        {/* Section Title */}
+        <Text style={styles.loadingTitle}>Generating Quiz...</Text>
 
-    return (
-      <SafeAreaView style={styles.homeScreenWrapper}>
+        {/* Title input descrition */}
+        <Text style={styles.loadingSubtitle}>This may take a moment</Text>
 
-        {/* Section Content */}
-        <View style={styles.sectionContent}>
-
-          {/* Section Title */}
-          <Text>Generating Quiz...</Text>
-
-          {/* Title input descrition */}
-          <Text>This may take a moment</Text>
-
-          {/* Progress bar */}
+        {/* Progress bar */}
+        <View style={{ ...styles.progressbarWrapper, ...styles.shadowDark }}>
           <Bar
             progress={progress}
+            height={38}
+            width={292}
+            color={COLOURS.loadingGreen}
+            unfilledColor={COLOURS.white}
+            borderRadius={70}
           />
         </View>
-      </SafeAreaView>
-    );
-  }
+      </View>
+    </SafeAreaView>
+  );
+}
 
-  // Styles
-  const styles = StyleSheet.create({
-    homeScreenWrapper: {
-      backgroundColor: COLOURS.settingsGreen,
-      height: "100%",
-      display: "flex",
-      justifyContent: "center",
-    },
+// Styles
+const styles = StyleSheet.create({
+  homeScreenWrapper: {
+    backgroundColor: COLOURS.settingsGreen,
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+  },
 
-    sectionContent: {
-      width: "100%",
-      paddingHorizontal: 100,
+  sectionContent: {
+    width: "100%",
+  },
+
+  loadingTitle: {
+    fontFamily: "HammersmithOne",
+    fontSize: 28,
+    textAlign: "center",
+  },
+
+  loadingSubtitle: {
+    fontFamily: "HammersmithOne",
+    fontSize: 18,
+    textAlign: "center",
+    opacity: 0.5,
+    marginTop: 8,
+  },
+
+  progressbarWrapper: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 36,
+  },
+
+  shadowDark: {
+    shadowColor: COLOURS.black,
+    shadowOffset: {
+      width: 0,
+      height: 7,
     },
-  });
+    shadowOpacity: 0.41,
+    shadowRadius: 9.11,
+
+    elevation: 14,
+  },
+});
